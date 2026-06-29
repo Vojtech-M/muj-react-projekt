b68e2f4d4e1847f8b0940060ccc98337bac1204f
Vojtěch Michal <vojtamichal04@gmail.com>
2026-06-29 15:21:29 +0200

    Add deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Vojtech-M/muj-react-projekt', // <-- SEM DOPIŠ PŘESNÝ NÁZEV SVÉHO GITHUB REPOZITÁŘE (včetně lomítek)
+  base: '/muj-react-projekt/', // <-- TADY MUSÍ BÝT JEN NÁZEV SLOŽKY V LOMÍTKÁCH, NE LINK NA GITHUB CODES!
 })
